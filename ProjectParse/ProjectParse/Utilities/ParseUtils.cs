d4d283e5fd1e1bf8c00a6739cf359dce7ca959f3
ByronMayne <ByronMayne@gmail.com>
2018-03-30 14:02:45 -0400

    Added ParseUtils to help edit strings.

---
+++ ProjectParse/ProjectParse/Utilities/ParseUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectParse.Utilities
+{
+    public static class ParseUtils
+    {
+        /// <summary>
+        /// Removes all leading white space from a string and returns the result.
+        /// </summary>
+        public static string RemoveLeadingWhiteSpace(string text)
+        {
+            int start = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!char.IsWhiteSpace(text[i]))
+                {
+                    break;
+                }
+                start++;
+            }
+            if (start > 0)
+            {
+                return text.Substring(start);
+            }
+            return text;
+        }
+    }
+}
