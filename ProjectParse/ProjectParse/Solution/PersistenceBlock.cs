8882728d905b3d73a19d00872652d818b38fd345
ByronMayne <ByronMayne@gmail.com>
2018-03-30 10:20:00 -0400

    Started development on persistenceblocks

---
+++ ProjectParse/ProjectParse/Solution/PersistenceBlock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ProjectParse.Solution
+{
+    internal struct PersistenceBlock
+    {
+        public string guid;
+        public string typeGuid;
+        public string instanceID;
+        public string path;
+
+        /// <summary>
+        /// Checks to see if a block equals this one.
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is PersistenceBlock))
+            {
+                return false;
+            }
+            PersistenceBlock block = (PersistenceBlock)obj;
+            return block == this;
+        }
+
+        /// <summary>
+        /// Gets the hashcode for this persistence block.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            int hash = 947402346;
+            unchecked
+            {
+                hash ^= guid.GetHashCode();
+                hash ^= typeGuid.GetHashCode();
+                hash ^= instanceID.GetHashCode();
+                hash ^= path.GetHashCode();
+            }
+            return hash;
+        }
+
+        public static bool operator ==(PersistenceBlock lhs, PersistenceBlock rhs)
+        {
+            bool isEqual = string.Equals(lhs.guid, rhs.guid, StringComparison.Ordinal);
+            isEqual &= string.Equals(lhs.typeGuid, rhs.typeGuid, StringComparison.Ordinal);
+            isEqual &= string.Equals(lhs.instanceID, rhs.instanceID, StringComparison.Ordinal);
+            isEqual &= string.Equals(lhs.path, rhs.path, StringComparison.Ordinal);
+            return isEqual;
+        }
+
+        public static bool operator !=(PersistenceBlock lhs, PersistenceBlock rhs)
+        {
+            bool isEqual = string.Equals(lhs.guid, rhs.guid, StringComparison.Ordinal);
+            isEqual &= string.Equals(lhs.typeGuid, rhs.typeGuid, StringComparison.Ordinal);
+            isEqual &= string.Equals(lhs.instanceID, rhs.instanceID, StringComparison.Ordinal);
+            isEqual &= string.Equals(lhs.path, rhs.path, StringComparison.Ordinal);
+            return !isEqual;
+        }
+    }
+}
---
+++ ProjectParse/ProjectParse/Solution/PersistenceBlocks.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectParse.Solution
+{
+    internal class PersistenceBlocks
+    {
+        /// <summary>
+        /// This list of all blocks included in this project;
+        /// </summary>
+        private IList<PersistenceBlock> _blocks;
+
+        /// <summary>
+        /// Adds a new persistence block to the solution.
+        /// </summary>
+        /// <returns>True if the block was added and false if it already existed.</returns>
+        public bool Add(PersistenceBlock block)
+        {
+            if (_blocks.Contains(block))
+            {
+                return false;
+            }
+
+            _blocks.Add(block);
+            return true;
+        }
+
+        /// <summary>
+        /// Removes a persistence block from the solution.
+        /// </summary>
+        /// <param name="block">THe block you want to remove</param>
+        /// <returns>True if the block was remove and false if it was not.</returns>
+        public bool Remove(PersistenceBlock block)
+        {
+            for (int i = 0; i < _blocks.Count; i++)
+            {
+                if(_blocks[i] == block)
+                {
+                    _blocks.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
