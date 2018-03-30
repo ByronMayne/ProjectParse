df2f20ac4bb1d60055294177a2237ca7cba5fbe3
ByronMayne <ByronMayne@gmail.com>
2018-03-30 12:41:50 -0400

    Completed parsing logic for persistence blocks. Still need to write
    unit tests.

--- ProjectParse/ProjectParse/SolutionContent/PersistenceBlock.cs
+++ ProjectParse/ProjectParse/SolutionContent/PersistenceBlock.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ProjectParse.SolutionContent
 {
@@ -45,7 +46,57 @@ namespace ProjectParse.SolutionContent
         /// <returns>The parsed block</returns>
         internal static PersistenceBlock Parse(string text)
         {
+            // Example: 
+            // Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "ProjectParseTests", "ProjectParseTests\ProjectParseTests.csproj", "{7E0C5121-B5D3-4A45-AACF-9623C87BADB9}"
             PersistenceBlock block = new PersistenceBlock();
+
+            // Create a reader 
+            StreamReader reader = new StreamReader(text);
+
+            int fieldIndex = 0;
+            int index = 0;
+            while(!reader.EndOfStream)
+            {
+                int start = 0;
+                int length = 0;
+                char c = (char)reader.Read();
+                
+                if(c == '"')
+                {
+                    start = index;
+                    do
+                    {
+                        index++;
+                        length++;
+                    } while (c == '"');
+
+                    if(length > 0)
+                    {
+                        string content = text.Substring(start, length);
+
+                        switch(fieldIndex)
+                        {
+                            case 0: block.typeGuid = content; break;
+                            case 1: block.instanceID = content; break;
+                            case 2: block.path = content; break;
+                            case 3: block.typeGuid = content; break;
+                        }
+                        fieldIndex++;
+                        if(fieldIndex > 3)
+                        {
+                            // All fields assigned 
+                            break;
+                        }
+                    }
+                }
+
+                index++;
+            }
+
+            if (fieldIndex >= 3)
+            {
+                throw new ParseException("Unable to parse all fields of Persistence Block. Expect four but found " + fieldIndex);
+            }
 
             return block;
         }
