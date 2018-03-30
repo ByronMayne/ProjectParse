f7113d8d85afd378cea51b5f01e1be0c3f02e5cc
ByronMayne <ByronMayne@gmail.com>
2018-03-30 10:51:38 -0400

    Added Unit test projects

---
+++ ProjectParse/ProjectParseTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectParseTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectParseTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e0c5121-b5d3-4a45-aacf-9623c87badb9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjectParse/ProjectParseTests/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using ProjectParse.SolutionContent;
+
+namespace ProjectParseTests
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        [Description("We should get an exception if we try to read a solution that does not exist.")]
+        public void ReadMissingException()
+        {
+            Exception thrownException = null;
+            try
+            {
+                Solution.Read("C://SolutionTests/InvalidSln.sln");
+            }
+            catch (Exception e)
+            {
+                thrownException = e;
+            }
+            string error = "We should have thrown a file not found exception as we sent an invalid path";
+            Assert.IsInstanceOfType(thrownException, typeof(FileNotFoundException), error);
+        }
+    }
+}
